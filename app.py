88a2721968357ce1e176aa7550056e7f3f31f948
Devices Focal <devices@focal.systems>
2020-03-29 23:43:52 -0700

    added cron and updateresults.py

--- app.py
+++ app.py
@@ -9,23 +9,25 @@ app = Flask(__name__)
 Bootstrap(app)
 
 def get_files(req_path):
-    BASE_DIR = '/app/data/csse_covid_19_data/csse_covid_19_daily_reports/'
+    try:
+        BASE_DIR = '/app/data/csse_covid_19_data/csse_covid_19_daily_reports/'
 
-    # Joining the base and the requested path
-    abs_path = os.path.join(BASE_DIR, req_path)
+        # Joining the base and the requested path
+        abs_path = os.path.join(BASE_DIR, req_path)
 
-    # Return 404 if path doesn't exist
-    if not os.path.exists(abs_path):
-        return abort(404)
+        # Return 404 if path doesn't exist
+        if not os.path.exists(abs_path):
+            return abort(404)
 
-    # Check if path is a file and serve
-    if os.path.isfile(abs_path):
-        return send_file(abs_path)
+        # Check if path is a file and serve
+        if os.path.isfile(abs_path):
+            return send_file(abs_path)
 
-    # Show directory contents
-    files = [i for i in os.listdir(abs_path) if i.endswith(".csv")]
-
-    return sorted(files)
+        # Show directory contents
+        files = [i for i in os.listdir(abs_path) if i.endswith(".csv")]
+        return sorted(files)
+    except Exception as e:
+        return []
 
 #@app.route('/data/<path:req_path>')
 @app.route("/", defaults={'req_path': ''})
--- task.sh
+++ task.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
-cd /app/data
-git pull origin master
-python /app/update.py
+BASEDIR=/app
+REPOSRC=https://github.com/CSSEGISandData/COVID-19.git
+LOCALREPO=$BASEDIR/data
+
+# We do it this way so that we can abstract if from just git later on
+LOCALREPO_VC_DIR=$LOCALREPO/.git
+
+if [ ! -d $LOCALREPO_VC_DIR ]
+then
+    git clone $REPOSRC $LOCALREPO
+else
+    cd $LOCALREPO
+    git pull $REPOSRC
+fi
+
+
+# Now rsync all the submissions
+GOOGLESTORAGEPATH=$BASEDIR/data/google_storage_data/
+if [ ! -d $GOOGLESTORAGEPATH ]
+then
+	mkdir $GOOGLESTORAGEPATH
+fi
+
+gsutil rsync -r gs://covid_website_data/ $GOOGLESTORAGEPATH
+
+python $BASEDIR/updateresults.py $GOOGLESTORAGEPATH $LOCALREPO
+# End
